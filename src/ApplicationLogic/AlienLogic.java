7d082ac6934e088a4dd5469a3577d56bdf1decb7
Ahmet <Ahmet@MSI-Ahmet-Yesevi>
2022-12-24 03:04:51 +0300

    Some alien features added

--- src/ApplicationLogic/AlienLogic.java
+++ src/ApplicationLogic/AlienLogic.java
@@ -1,12 +1,61 @@
 package ApplicationLogic;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import UI.GameFrame;
+import UI.GamePanel;
+import dataStructures.Location;
+import domain.Key;
+import domain.aliens.*;
 
 public class AlienLogic {
 	GameFrame gameFrame;
 	private final Random r = new Random();
+	
+	public AlienLogic(GameFrame gameFrame) {
+		super();
+		this.gameFrame = gameFrame;
+	}
+	
+	public Boolean ChangeLocationOfKey(Alien alien, Key key) {
+		
+		return false;
+		
+	}
+	
+	
+	public TimeWastingAlien getTimeWastingAlien() {
+		Location doorLocation = gameFrame.getDoorLocation();
+		int rowCount = gameFrame.getNumRow();
+		int columnCount = gameFrame.getNumCol();
+		Boolean flag = false;
+		ArrayList<Location> object_locations = new ArrayList<>(); 
+		for (int i = 0; i < rowCount; i++) {
+			  for ( int j = 0; j < columnCount ; j++) {
+				  if (i == 0 && j == 5) {
+					  continue;
+				  }
+				  else if (i == doorLocation.getLocationX() && j == doorLocation.getLocationY()) {
+					  continue;
+				  }
+				  if (GamePanel.getGameMap()[i][j].getIcon() != null) {
+					  if (i == 0 && j == 5) {
+						  continue;
+					  }
+					  object_locations.add(new Location(i,j));
+					  
+				  }
+			  }
+			}
+		
+		int rand = r.nextInt(object_locations.size());
+		TimeWastingAlien timeWastingAlien = new TimeWastingAlien(object_locations.get(rand));
+		return timeWastingAlien;
+		
+	}
+	
+	
 	
 	
 	
