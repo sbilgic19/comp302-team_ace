8f3ca8ff8ac75de13b748df76c21f89e2e7987d6
aoktay19 <aoktay19@ku.edu.tr>
2023-01-06 14:07:11 +0300

    Power ups trigger effect specifications added.

--- src/domain/powerUps/WearVestBehavior.java
+++ src/domain/powerUps/WearVestBehavior.java
@@ -14,6 +14,22 @@ public class WearVestBehavior implements PowerUpBehavior{
         this.player = player;
     }
 
+    /**
+     * Player wears protection vest that protects it for a one shoot.
+     *
+     * REQUIRES:
+     * - The player must be in a game level that has power up items.
+     * - The player must be able to collect power up items by clicking.
+     * - The player must be click the P button.
+     *
+     * MODIFIES:
+     * - The player isProtected be true.
+     * - It creates timer to keep time of the duration of the protection vest
+     * - It removes the protection vest power up from the player's bag.
+     *
+     * EFFECTS:
+     * - The effect lasts for 20 seconds.
+     */
     @Override
     public void performBehavior() {
         player.setIsProtected(true);
